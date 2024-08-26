308439de2f384e0868b02b151699e454f8533bf8
Valerii Reutskyi <86915685+gifites555@users.noreply.github.com>
2024-08-27 02:01:59 +0300

    Got s good movement
    Got s good movement

--- Source/TheNewUEProject/Private/GifCharacter.cpp
+++ Source/TheNewUEProject/Private/GifCharacter.cpp
@@ -1,69 +1,88 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GifCharacter.h"
-#include "Camera/CameraComponent.h"	
+#include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "Kismet/KismetMathLibrary.h"
+
 
 // Sets default values
 AGifCharacter::AGifCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
-	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
-	SpringArmComp->SetupAttachment(RootComponent);
+    SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+    SpringArmComp->SetupAttachment(RootComponent);        
 
-	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
-	CameraComp->SetupAttachment(SpringArmComp);
+    CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+    CameraComp->SetupAttachment(SpringArmComp);
 
-	bUseControllerRotationPitch = true;
+    SpringArmComp->bUsePawnControlRotation = true;
+    SpringArmComp->bInheritPitch = true;
+    SpringArmComp->bInheritYaw = true;
+    SpringArmComp->bInheritRoll = true;
+
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+
+    bUseControllerRotationYaw = false;
+    
+   
 }
 
 // Called when the game starts or when spawned
 void AGifCharacter::BeginPlay()
 {
-
-
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 void AGifCharacter::MoveForward(float Value)
 {
-	AddMovementInput(GetActorForwardVector(), Value);
+    if (Controller && Value != 0.0f)
+    {
+        FRotator CameraRotation = Controller->GetControlRotation();
 
+        
+        FRotator YawRotation(0, CameraRotation.Yaw, 0);
+        FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 
+        
+        AddMovementInput(Direction, Value);
+    }
 }
 
 void AGifCharacter::MoveRAL(float Value)
 {
-	AddMovementInput(GetActorRightVector(), Value);
+    if (Controller && Value != 0.0f)
+    {
+        
+        FRotator CameraRotation = Controller->GetControlRotation();
+        
+       
+        FRotator YawRotation(0, CameraRotation.Yaw, 0);
+        FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
-
+        AddMovementInput(Direction, Value);
+    }
 }
-
-
-
 
 // Called every frame
 void AGifCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 
+   
 }
 
 // Called to bind functionality to input
 void AGifCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AGifCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRAL", this, &AGifCharacter::MoveRAL);
+    PlayerInputComponent->BindAxis("MoveForward", this, &AGifCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRAL", this, &AGifCharacter::MoveRAL);
 
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	//PlayerInputComponent->BindAxis("Up", this, &APawn::AddControllerPitchInput);
-
-
+  
+    PlayerInputComponent->BindAxis("Turn", this, &AGifCharacter::AddControllerYawInput);
+    PlayerInputComponent->BindAxis("Up", this, &AGifCharacter::AddControllerPitchInput);
 }
-
